7930e6acbebab9e100ef012436154fc6844dfc3d
dhiraj19999 <yogeshsamse682@gmail.com>
2024-03-20 11:40:08 +0530

    add

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
   res.send("hello from server");
 });
 server.listen(PORT, () =>
